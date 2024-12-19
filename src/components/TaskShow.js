d08eab9f565483ff6c59ddf84e92f78597d8e094
hande <handeeeozzzz@gmail.com>
2024-12-19 23:02:16 +0300

    taskcreatededüzenlemeformueklendi

--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -1,69 +1,38 @@
-import React, { useState } from 'react'
+import { useState } from 'react';
+import TaskCreate from './TaskCreate';
 
-const TaskShow = ({task,onDelete,onEdit,onchangeform}) => {
-  const [isEdit, setIsEdit] = useState(false);
-  const [changetaskDesc,setchangeTaskDesc]=useState("");
-  const [changetitle,setchangetitle]=useState("");
-const taskDelete=()=>{
-onDelete(task.id);
-}
-const taskedit=()=>{
-  onEdit(task.title,task.taskDesc);
-  setIsEdit(true);
+function TaskShow({ task, onDelete }) {
+  const [showEdit, setShowEdit] = useState(false);
+  const handleDeleteClick = () => {
+    onDelete(task.id);
+  };
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
 
-}
-const handleChangetask=(event)=>{
-  setchangeTaskDesc(event.target.value);
-  setchangetitle(event.target.value);
-
-  onchangeform(changetitle,changetaskDesc,task.id)
-}
-const handleEditsubmit=()=>{
-
-}
+  console.log(task);
   return (
     <div className="task-show">
-    {!isEdit ? (
-      // Normal Gösterim
-      <>
-        <h3 className="task-title">Göreviniz</h3>
-        <p>{task.title}</p>
-        <h3 className="task-title">Yapılacaklar</h3>
-        <p>{task.taskDesc}</p>
+      {showEdit ? (
+        <TaskCreate task={task} taskformUpdate={true} />
+      ) : (
         <div>
-          <button className="task-delete" onClick={taskDelete}>
-            Sil
-          </button>
-          <button className="task-edit" onClick={taskedit}>
-            Güncelle
-          </button>
+          <h3 className="task-title">Göreviniz</h3>
+          <p>{task.title}</p>
+          <h3 className="task-title">Yapılacaklar</h3>
+          <p>{task.taskDesc}</p>
+          <div>
+            <button className="task-delete" onClick={handleDeleteClick}>
+              Sil
+            </button>
+            <button className="task-edit" onClick={handleEditClick}>
+              Güncelle
+            </button>
+          </div>
         </div>
-      </>
-    ) : (
-      // Düzenleme Formu
-      <form className="task-form" onSubmit={handleEditsubmit}>
-        <label className="task-label">Başlığı düzenleyiniz</label>
-        <input
-          className="task-input"
-          name="title"
-          value={changetitle}
-          onChange={handleChangetask}
-        />
-        <label className="task-label">Task düzenleyiniz</label>
-        <textarea
-          className="task-input"
-          name="taskDesc"
-          rows={5}
-          value={changetaskDesc}
-          onChange={handleChangetask}
-        />
-        <button className="task-button" type="submit">
-          Düzenle
-        </button>
-      </form>
-    )}
-  </div>
-  )
+      )}
+    </div>
+  );
 }
 
-export default TaskShow
+export default TaskShow;
